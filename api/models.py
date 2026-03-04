bae94110e0c7ecbfde24de886d9f0f160b350c85
shishirgautam939-tech <shishirgautam939@gmail.com>
2026-03-04 09:03:28 +0545

    created DRF serializer with validation

--- api/models.py
+++ api/models.py
@@ -1,12 +1,12 @@
 from django.db import models
 
-class Note(models.Model):
-    title = models.CharField(max_length=100)
-    description = models.TextField()
+class GroceryItem(models.Model):
+    name = models.CharField(max_length=200)
+    completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.title
+        return self.name
 
     class Meta:
-        ordering = ['-id']
+        ordering = ['-created_at']
